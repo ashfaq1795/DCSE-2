0fdcd529057596d70534d80d3c1e25d45f562717
5ddeveloper247 <127947055+5ddeveloper247@users.noreply.github.com>
2023-10-09 15:00:24 +0500

    Revert "8th push1"
    This reverts commit a41007ebcbee6ed9d5d35faff79a4c479c5de339.

--- 8th semester/Web Eng/Assignments/Assignment02 files/task02.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $(window).on('resize',function(){
-        const windowwidth=$(window).width();
-        const fontsize=windowwidth/20;
-        $('#size').css('font-size',fontsize +'px');
-    });
-})
--- 8th semester/Web Eng/Tasks/lecture_5.1_php/selectphp.php
+++
@@ -1,9 +0,0 @@
-<?php
-    foreach ($_POST['color'] as $selected) {
-        if($selected=='none'){
-            echo 'You have not select any your favorate color.';
-        } else{
-        echo 'Your Favorate color is '.$selected;
-    }
-}
-?>
